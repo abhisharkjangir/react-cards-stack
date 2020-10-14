87f2b33cb19ee3088ae445f4eff52f9d88e1d568
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-10-14 10:24:32 +0200

    feat: simple demo of cards in action

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,20 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { CardStack } from '../.';
+import '../dist/stack.css';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <CardStack
+        infinite
+        images={[
+          'https://picsum.photos/id/237/200/300',
+          'https://picsum.photos/id/233/200/300',
+          'https://picsum.photos/id/230/200/300',
+        ]}
+      />
     </div>
   );
 };
